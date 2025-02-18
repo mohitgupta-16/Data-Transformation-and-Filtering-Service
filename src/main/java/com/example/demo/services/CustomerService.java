6544123bc88500afbb40d66a021ffe51b1f62416
Mohit Gupta <mohitgupta.mg2727@gmail.com>
2025-02-18 09:28:29 +0530

    lookUpPlan

--- src/main/java/com/example/demo/services/CustomerService.java
+++ src/main/java/com/example/demo/services/CustomerService.java
@@ -40,7 +40,7 @@ public class CustomerService {
     @PostConstruct
     public void init() throws IOException {
         System.out.println(mongoUri + "========="+ dbName);
-        Collection<Function> functions = List.of(new LookUpFunction(mongoUri, dbName), new YearsTillNow());
+        Collection<Function> functions = List.of(new LookUpFunction(mongoUri, dbName), new YearsTillNow(), new PlanLookUpFunction(mongoUri, dbName));
         jsltExpression = Parser.compile(new File("src/main/resources/templates/transform-customer.jslt"), functions);
     }
 
@@ -116,7 +116,6 @@ public class CustomerService {
             MongoCollection<Document> collection = mongoClient.getDatabase(dbName).getCollection(collectionName);
 
             Document document = collection.find(eq(filterKey, filterValue)).first();
-
             if (document != null && document.containsKey(outputField)) {
                 return new TextNode(document.getString(outputField));
             }
@@ -157,6 +156,54 @@ public class CustomerService {
             } catch (Exception e) {
                 throw new IllegalArgumentException("Invalid date or date format", e);
             }
+        }
+    }
+    
+    public static class PlanLookUpFunction implements Function {
+        
+        private final MongoClient mongoClient;
+        private final String dbName;
+        
+        public PlanLookUpFunction(String connectionString, String dbName) {
+            this.mongoClient = MongoClients.create(connectionString);
+            this.dbName = dbName;
+        }
+        
+        @Override
+        public String getName() {
+            return "PlanLookup";
+        }
+        
+        @Override
+        public int getMinArguments() {
+            return 3;
+        }
+        
+        @Override
+        public int getMaxArguments() {
+            return 3;
+        }
+        
+        @Override
+        public JsonNode call(JsonNode input, JsonNode[] params) {
+            String collectionName = params[0].asText();
+            JsonNode filterNode = params[1];
+            String outputField = params[2].asText();
+            
+            String filterKey = filterNode.fieldNames().next();
+            String filterValue = filterNode.get(filterKey).asText();
+            
+            MongoCollection<Document> collection = mongoClient.getDatabase(dbName).getCollection(collectionName);
+            
+            Document document = collection.find(eq(filterKey, filterValue)).first();
+            
+            System.out.println(document);
+            
+            if (document != null && document.containsKey(outputField)) {
+                return new TextNode(document.getString(outputField));
+            }
+            
+            return new TextNode("null");
         }
     }
 
